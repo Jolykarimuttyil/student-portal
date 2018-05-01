757fb139395ac5e9a4690a083cf57fe18242e2ae
Joly <jolykrmkttl1@gmail.com>
2018-04-30 19:47:21 -0500

    add login feature

--- public/index.js
+++ public/index.js
@@ -6,6 +6,7 @@ var HomePage = {
     return {
       message: "Joe!",
 
+
       command: "Get going !",
 
       message1: "It's Richgang"
@@ -16,8 +17,45 @@ var HomePage = {
   computed: {}
 };
 
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+      .post("/user_token", params)
+      .then(function(response) {
+        axios.defaults.headers.common["Authorization"] =
+        "Bearer " + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
+        router.push("/");
+      })
+      .catch(
+        function(error) {
+          this.errors = ["Invalid email or password."];
+          this.email = "";
+          this.password = "";
+        }.bind(this)
+        );
+    }
+  }
+};
+
+
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    {path: "/", component: HomePage },
+    {path: "/login", component: LoginPage }],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
@@ -25,5 +63,11 @@ var router = new VueRouter({
 
 var app = new Vue({
   el: "#vue-app",
-  router: router
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
 });
