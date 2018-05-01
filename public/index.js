7409fdf81fbfacdfa633e9077d76bea45ee7a361
Richard Song <richardsong18@gmail.com>
2018-04-30 20:25:21 -0500

    Show student attributes on homepage

--- public/index.js
+++ public/index.js
@@ -4,19 +4,26 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Joe!",
-
-
-      command: "Get going !",
-
-      message1: "It's Richgang"
+      message: "This is your info: ",
+      student: {
+        first_name: "Richard",
+        last_name: "Gang",
+        email: "richard@email.com",
+        phone_number: "1112223333",
+        bio: "Up and coming underground rapper.",
+        linkedin_url: "www.linkedin.com/in/richard-gang",
+        twitter_handle: "www.twitter.com/richard-gang",
+        personal_url: "www.richard-gang.com",
+        resume_url: "www.richard-gang-resume.com",
+        github_url: "www.github.com/richard-gang",
+        photo: "www.richard-gang-photo.com"
+      }
     };
   },
   created: function() {},
   methods: {},
   computed: {}
 };
-
 
 var LoginPage = {
   template: "#login-page",
@@ -33,29 +40,29 @@ var LoginPage = {
         auth: { email: this.email, password: this.password }
       };
       axios
-      .post("/user_token", params)
-      .then(function(response) {
-        axios.defaults.headers.common["Authorization"] =
-        "Bearer " + response.data.jwt;
-        localStorage.setItem("jwt", response.data.jwt);
-        router.push("/");
-      })
-      .catch(
-        function(error) {
-          this.errors = ["Invalid email or password."];
-          this.email = "";
-          this.password = "";
-        }.bind(this)
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
         );
     }
   }
 };
 
-
 var router = new VueRouter({
   routes: [
-    {path: "/", component: HomePage },
-    {path: "/login", component: LoginPage }],
+    { path: "/", component: HomePage },
+    { path: "/login", component: LoginPage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
